876687b8f2a8bf1b7ce57485bb37665e07e0aada
Roman Zhyliak <romka.if87@gmail.com>
2018-12-13 21:04:13 +0200

    Add table

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,6 +2,34 @@
 
 @section('content')
     <h2>USERS</h2>
+
+    <p>
+        <a href="{{route('users.create')}}" class="btn btn-success">Create new User</a>
+    </p>
+
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($users as $user)
+            <tr>
+                <th scope="row">{{$user->id}}</th>
+                <td>{{$user->name}}</td>
+                <td>{{$user->email}}</td>
+                <td>
+                    <a href="#" class="btn btn-primary">Edit</a>
+                    <a href="#" class="btn btn-danger">Delete</a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
     <ul>
         @foreach($users as $user)
             <li> {{ $user->name }}
